dc48c3bc771cba9c2e7fd686cce66ec04e677d53
TimothyNicdao <tmnicdao@hawaii.edu>
2021-11-03 17:52:07 -1000

    added bootstrap code

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default NavBar;
