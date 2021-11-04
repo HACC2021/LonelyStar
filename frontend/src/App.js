16020ab7fcc0f0e9faf80e2efa9215a8ad6f9c0a
TimothyNicdao <tmnicdao@hawaii.edu>
2021-11-03 16:36:55 -1000

    prepared react for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
@@ -17,6 +18,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <Route path="products" component = {Products}></Route>
+          <Route path="products" component = {Products}></Route>
+          <Route path="products" component = {Products}></Route>
+        </div>
+
       </header>
     </div>
   );
