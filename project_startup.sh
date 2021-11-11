77de3809fbd6ebf1f78d7e5fdd6e867317ad726d
TimothyNicdao <tmnicdao@hawaii.edu>
2021-11-10 23:35:38 -1000

    fixing startup script

--- project_startup.sh
+++ project_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -p 3000:3000 -it lonelystar bash client_startup.sh
-docker run -p 8000:8000 -it lonelystar bash server_startup.sh
+docker run -p 3000:3000 -it lonelystar bash client_startup.sh &
+docker run -p 8000:8000 -it lonelystar bash server_startup.sh &
--- server_startup.sh
+++ server_startup.sh
@@ -2,6 +2,6 @@
 cd languageModel
 python3 train_model.py
 cd ../backend
-python3 manage.py runserver
+python3 manage.py runserver 0.0.0.0:8000
 
 
