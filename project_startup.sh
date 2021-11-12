e15cb249559ad6cc01f6cc64a05e015c35d43a80
TimothyNicdao <tmnicdao@hawaii.edu>
2021-11-12 12:00:58 -1000

    pdated startup script

--- project_startup.sh
+++ project_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull timothynicdao/lonelystar
-docker run -i -p 3000:3000 lonelystar bash client_startup.sh &
-docker run -i -p 8000:8000 lonelystar bash server_startup.sh &
+docker run -i -p 3000:3000 timothynicdao/lonelystar bash client_startup.sh &
+docker run -i -p 8000:8000 timothynicdao/lonelystar bash server_startup.sh &
